d518103fb59789b0f3dbad2ab614b8743786140a
André Lucas <andrelucca99@gmail.com>
2024-02-10 13:23:45 -0300

    fix: remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-// import styled from 'styled-components';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -7,17 +6,6 @@ import NotFound from './pages/NotFound';
 import Layout from './components/layout';
 
 function App() {
-  // const AppStyled = styled.div`
-  //   background-color: ${(props) => props.theme.bgColor};
-  //   color: #fff;
-  //   width: 100%;
-  //   height: 100%;
-  //   display: flex;
-  //   align-items: center;
-  //   justify-content: center;
-  //   flex-direction: column;
-  // `;
-
   return (
     <Routes>
       <Route path="/" element={ <Layout /> }>
