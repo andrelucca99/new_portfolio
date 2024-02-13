68e99a8b4121f7d3ffe31c181a0fa3d42ddc5370
André Lucas <andrelucca99@gmail.com>
2024-02-13 10:48:49 -0300

    style: remove styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -56,24 +56,6 @@ const SectionProjects = styled.section`
   gap: 10px;
 `;
 
-const LinkProjects = styled(Link)`
-  text-decoration: none;
-  list-style: none;
-  background-color: transparent;
-  transition: all 0.2s ease-out;
-  padding: 5px 20px;
-  color: ${({ theme }) => theme.textColor.primary};
-  border: 1px solid ${({ theme }) => theme.textColor.segundary};
-  border-radius: 5px;
-  margin: 20px 0;
-
-  &:hover {
-    cursor: pointer;
-    color: ${({ theme }) => theme.bgColor};
-    background-color: ${({ theme }) => theme.bgCardTech};
-  }
-`;
-
 const SectionArticles = styled.article`
   width: 100%;
   max-width: 20%;
@@ -107,9 +89,6 @@ function Home() {
                 link={ item.link }
               />
             ))}
-          {/* <LinkProjects to="/projetos">
-            Mais Projetos
-          </LinkProjects> */}
           <Button text="Mais Projetos" url="/projetos" />
         </SectionProjects>
         <SectionArticles>
