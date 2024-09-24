09a31ecbea80283be9f109f0bee0a1bee6b27fc6
André Lucas <andrelucca99@gmail.com>
2024-09-24 12:56:10 -0300

    refactor: separando importações da página sobre

---
+++ src/pages/About/utils/index.ts
@@ -0,0 +1,27 @@
+import css from '../assets/css.png';
+import html from '../assets/html.png';
+import jest from '../assets/jest.png';
+import node from '../assets/nodejs.png';
+import react from '../assets/react.png';
+import angular from '../assets/angular.png';
+import javascrit from '../assets/javascript.png';
+import typescrit from '../assets/typescript.png';
+import c_sharp from '../assets/c-sharp.png';
+import redux from '../assets/redux.png';
+import api from '../assets/api.png';
+import docker from '../assets/docker.png';
+import jwt from '../assets/jwt.png';
+import mongodb from '../assets/mongodb.png';
+import python from '../assets/python.png';
+import sql from '../assets/sql.png';
+import testingLibrary from '../assets/testing-library.png';
+import c from '../assets/c.png';
+import trybe from '../assets/trybe.png';
+import uninove from '../assets/uninove.png';
+import _42_sp from '../assets/42sp.png';
+
+export {
+  css, _42_sp, angular, api, c, c_sharp, docker, redux,
+  html, javascrit, jest, jwt, mongodb, node, python, react,
+  sql, testingLibrary, trybe, typescrit, uninove,
+};
--- src/pages/About/utils/mock.ts
+++ src/pages/About/utils/mock.ts
@@ -1,26 +1,10 @@
-import css from '../assets/css.png';
-import html from '../assets/html.png';
-import jest from '../assets/jest.png';
-import node from '../assets/nodejs.png';
-import react from '../assets/react.png';
-import angular from '../assets/angular.png';
-import javascrit from '../assets/javascript.png';
-import typescrit from '../assets/typescript.png';
-import c_sharp from '../assets/c-sharp.png';
-import redux from '../assets/redux.png';
-import api from '../assets/api.png';
-import docker from '../assets/docker.png';
-import jwt from '../assets/jwt.png';
-import mongodb from '../assets/mongodb.png';
-import python from '../assets/python.png';
-import sql from '../assets/sql.png';
-import testingLibrary from '../assets/testing-library.png';
-import c from '../assets/c.png';
-import trybe from '../assets/trybe.png';
-import uninove from '../assets/uninove.png';
-import _42_sp from '../assets/42sp.png';
-
 import { ICardAcademic, ITechSkills } from './Interfaces';
+import {
+  html, css, javascrit, react, redux, angular,
+  typescrit, jest, testingLibrary, node, c,
+  c_sharp, api, docker, jwt, python, sql,
+  mongodb, _42_sp, trybe, uninove,
+} from '.';
 
 export const techSkills: Array<ITechSkills> = [
   {
